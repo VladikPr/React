9d9c209b661b97ca9c0d43daa1e512b2046bfea6
VladikPr <63455099+VladikPr@users.noreply.github.com>
2020-08-13 19:10:28 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './normalize.css';
+import './style.css';
+
+/* const Header = ({ text }) => <h1 id="hi">{text}</h1>;
+
+const Element = <p>Paragraph</p>;
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div className="main">
+                <Header text={'Text properties'}/>
+                {Element}
+            </div>
+        )
+    }
+} */
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
